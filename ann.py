701b8e8ce1a82d9621499bded63f3c923bec59ad
Jeff Ames <jeff.ames@cs.rutgers.edu>
2015-10-28 17:58:54 -0400

    Switch network to object tracking parameters
    - with just one training and one test value for now

--- ann.py
+++ ann.py
@@ -4,31 +4,50 @@ from pyfann import libfann
 
 connection_rate = 1
 learning_rate = 0.7
-num_input = 2
+num_input = 200
 num_hidden = 4
-num_output = 1
+num_output = 4
 
 desired_error = 0.0001
 max_iterations = 100000
 iterations_between_reports = 1000
+
+# one test input value
+input = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+         1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+
+         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+         0.1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 def trainNet():
     ann = libfann.neural_net()
     ann.create_sparse_array(connection_rate, (num_input, num_hidden, num_output))
     ann.set_learning_rate(learning_rate)
     ann.set_activation_function_output(libfann.SIGMOID_SYMMETRIC_STEPWISE)
-    ann.train_on_file("xor.data", max_iterations, iterations_between_reports, desired_error)
-    ann.save("xor.net")
+    ann.train_on_file("objtrack.data", max_iterations, iterations_between_reports, desired_error)
+    ann.save("objtrack.net")
 
 def testValue(inputs):
     ann = libfann.neural_net()
-    ann.create_from_file("xor.net")
+    ann.create_from_file("objtrack.net")
     return ann.run(inputs)
 
 if __name__ == "__main__":
     trainNet()
-    print(testValue([-1, -1]))
-    print(testValue([-1, 1]))
-    print(testValue([1, -1]))
-    print(testValue([1, 1]))
-    print(testValue([0.5, 0.5]))
+    print(testValue(input))
