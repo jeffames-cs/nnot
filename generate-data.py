6a7a553dd51abbd6ade2e448bae0e4e2a8036f23
Jeff Ames <jeff.ames@cs.rutgers.edu>
2015-11-06 11:37:54 -0500

    Add separate left and right eye data generation

--- generate-data.py
+++ generate-data.py
@@ -6,10 +6,12 @@ from nott_params import *
 num_samples = int(gridDim[0] * gridDim[1] * 10)
 
 def generate_data(numx, numy):
-    data = ['0' for i in range(numx * numy)]
+    ldata = ['0' for i in range(numx * numy)]
+    rdata = ['0' for i in range(numx * numy)]
     stimulus = (random.randint(0, numx - 1), random.randint(0, numy - 1))
-    data[stimulus[1] * numx + stimulus[0]] = '1'
-    return data, stimulus
+    ldata[stimulus[1] * numx + stimulus[0]] = '1'
+    rdata[stimulus[1] * numx + stimulus[0]] = '1'
+    return ldata, rdata, stimulus
 
 def print_header():
     print("{0} {1} {2}".format(num_samples, num_inputs, num_outputs))
@@ -22,8 +24,8 @@ if __name__ == '__main__':
     random.seed()
     print_header()
     for i in range(num_samples):
-        data, stimulus = generate_data(gridDim[0], gridDim[1])
-        print_input(data, data) # duplicate for two eyes
+        ldata, rdata, stimulus = generate_data(gridDim[0], gridDim[1])
+        print_input(ldata, rdata)
         scaled_x = 2 * float(stimulus[0]) / gridDim[0] - 1
         scaled_y = 2 * float(stimulus[1]) / gridDim[1] - 1
         print("{0} {1} {2} {3}".format(scaled_x, scaled_y,
